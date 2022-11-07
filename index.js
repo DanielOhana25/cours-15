bfb514f24f7288712f000ff4c4c674f03cdbaf00
DanielOhana25 <every.society@gmail.com>
2022-11-08 00:45:36 +0200

    lesson20

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 class Users {
-  #books;
+  books;
   constructor(name, lastName, city) {
     this.name = name;
     this.lastName = lastName;
     this.city = city;
-    this.#books = [];
+    this.books = [];
   }
   addBook(bookName) {
-    this.#books.push(bookName);
+    this.books.push(bookName);
   }
-  printBooks() {
-    this.#books.forEach((element) => {
-      console.log(element);
-    });
-  }
-}
-
-class Books {
-  constructor() {}
 }
 
 class Bilan {
-  constructor() {}
+  constructor() {
+    this.user = [];
+  }
+  addUser(user) {
+    this.user.push(user);
+  }
+  printArrBooks() {
+    for (let i = 0; i < this.user.length; i++) {
+      console.log(this.user[i].books);
+    }
+  }
 }
 let a = new Users("daniel", "Ohana", "Netanya");
+let b = new Users("shany", "Ohana", "Netanya");
 a.addBook("genesis");
 a.addBook("exodus");
 a.addBook("levitic");
 a.addBook("numbers");
 a.addBook("deteronome");
-a.printBooks();
-console.log(a);
+b.addBook("shmouel");
+b.addBook("iyov");
+b.addBook("Ovadia");
+b.addBook("Daniel");
+b.addBook("Yoel");
+
+let bilan1 = new Bilan();
+
+bilan1.addUser(a);
+bilan1.addUser(b);
+bilan1.printArrBooks();
